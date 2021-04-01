d9e3f98c95c0097780a861430fb9a4811625f62b
Matti Nelimarkka <matti.nelimarkka@helsinki.fi>
2021-04-01 21:06:54 +0300

    Initial code to query and search by organisation

---
+++ src/pyhy/courses.py
@@ -0,0 +1,53 @@
+import requests
+import datetime
+
+_API_PATH = 'https://studies.helsinki.fi/api/search'
+
+def _collect( query ):
+
+    page = 0
+    content = []
+
+    while True:
+
+        query['page'] = page
+
+        r = requests.get( _API_PATH , params = query )
+        data = r.json()
+
+        content += data['hits']
+        page += 1
+
+        if len( content ) >= data['totalHits']:
+            break
+
+    return content
+
+## a bit weird heuristic
+def _guess_study_year():
+    now = datetime.datetime.now()
+
+    if now.month < 7:
+        return now.year - 1
+    else:
+        return now.year
+
+def search( search = '', lang = 'en', academic_year = _guess_study_year() ):
+    return _collect( {'searchText': search, 'studyYear' : academic_year, 'lang' : lang } )
+
+def by_organisation( organisations = [], lang = 'en', academic_year = _guess_study_year() ):
+    ## todo: can requests multiple organisations at the same time, but not easy to plug in with the collect implementation
+    ret = []
+
+    for org in organisations:
+
+        org = str( org )
+
+        if not org.startswith( 'hy-org-' ):
+            org = 'hy-org-' + org
+
+        ret += _collect( {'organisation': org, 'studyYear' : academic_year, 'lang' : lang } )
+
+    return ret
+
+print( by_organisation( [116738259] ) )
